3ef670205925de579dd08331ecdf7db1f9ec11dd
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-26 08:00:58 +0300

    initial commit with ts types and variables

---
+++ types.ts
@@ -0,0 +1,16 @@
+const today = new Date();
+console.log(today.getMonth());
+// today.day
+
+
+const person = {
+    age: 20
+}
+//person.sdfsdf;
+
+class Color {
+
+}
+
+const red = new Color();
+
---
+++ variables.ts
@@ -0,0 +1,23 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//build in objects
+let now: Date = new Date();
+
+//arrays
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false];
+
+//classes
+class Car {}
+let car: Car = new Car();
+
+//object literal
+let point: { x: number; y: number } = {
+    x: 123,
+    y: 20
+}
