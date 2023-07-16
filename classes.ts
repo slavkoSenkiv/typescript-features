a384a4ba849c5529b634b7052074dc6d6287e363
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-16 09:26:28 +0300

    play with public vs private vs protected

--- classes.ts
+++ classes.ts
@@ -1,22 +1,30 @@
 class Vehicle {
-    drive(): void {
+
+    /* public drive(): void {
         console.log('chugga chugga');
-    }
-    honk(): void {
+    } */
+
+    protected honk(): void {
         console.log('beep');
     }
 }
 
 class Car extends Vehicle {
-    drive(): void {
+
+    private drive(): void {
         console.log('vroom');
+    }
+
+    startDrivingProcess(): void {
+        this.drive();
+        this.honk();
     }
 }
 
 const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+/* vehicle.drive(); */
+//vehicle.honk();
 
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
+//car.honk();
