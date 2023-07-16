59174dd4cf5f4c60b71f308ea3adb5b8954a74b6
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-07-16 09:46:38 +0300

    finish classes

--- classes.ts
+++ classes.ts
@@ -1,15 +1,18 @@
 class Vehicle {
-
-    /* public drive(): void {
-        console.log('chugga chugga');
-    } */
+    constructor(public color: string) {}
 
     protected honk(): void {
         console.log('beep');
     }
 }
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
 class Car extends Vehicle {
+    
+    constructor(public wheels: number, color: string){
+        super(color);
+    }
 
     private drive(): void {
         console.log('vroom');
@@ -21,10 +24,5 @@ class Car extends Vehicle {
     }
 }
 
-const vehicle = new Vehicle();
-/* vehicle.drive(); */
-//vehicle.honk();
-
-const car = new Car();
+const car = new Car(4, 'red');
 car.startDrivingProcess();
-//car.honk();
